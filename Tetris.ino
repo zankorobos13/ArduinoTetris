603fbc40563f5a7412cf591585ad9a24cb5e06a4
zankorobos13 <aruseniys13@yandex.ru>
2026-01-06 04:37:43 +0300

    Система динамической индикации перенесена в отдельную функцию

--- Tetris.ino
+++ Tetris.ino
@@ -5,33 +5,21 @@ const int PWM_period_ms = 2;
 bool LED_matrix[LED_matrix_size_y][LED_matrix_size_x] = {
   {true, true, true},
   {false, true, false},
-  {true, false, true}
+  {false, true, false}
 };
 
 uint8_t LED_column_pins[LED_matrix_size_x] = {2, 3, 4};
 uint8_t LED_row_pins[LED_matrix_size_y] = {5, 6, 7};
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
-}
-
 int current_step_on_columns = 0;
-unsigned long long prev_ms = 0;
+unsigned long long prev_ms_LED_matrix_control = 0;
 unsigned long long ms = 0;
 
-void loop() {
-  ms = millis();
-    
+void LED_matrix_control(){
   if(current_step_on_columns >= LED_matrix_size_x){
     current_step_on_columns = 0;
   }
-  if (ms - prev_ms >= PWM_period_ms){
+  if (ms - prev_ms_LED_matrix_control >= PWM_period_ms){
     if (current_step_on_columns == 0){
       digitalWrite(LED_column_pins[LED_matrix_size_x - 1], LOW);
     }
@@ -50,6 +38,24 @@ void loop() {
     }
 
     current_step_on_columns++;
-    prev_ms = ms;
+    prev_ms_LED_matrix_control = ms;
   }
+}
+
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+}
+
+
+
+void loop() {
+  ms = millis();
+  LED_matrix_control();
 }
