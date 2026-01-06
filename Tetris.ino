ffa26ce69c7b5d5cee55112ac9956ec4107eeffd
zankorobos13 <aruseniys13@yandex.ru>
2026-01-06 03:21:15 +0300

    Часть системы динамической индикации на светодиодной матрице. Реализовано попеременное включение тразисторов отвечающих за замыкание цепи в столбцах матрицы. Работа со временем теперь реализуется через таймеры а не через delay()

--- Tetris.ino
+++ Tetris.ino
@@ -1,23 +1,46 @@
-void LED_arr_by_delay(uint8_t LED_pins[], int delta_time ){
-  digitalWrite(LED_pins[0], HIGH);
-  delay(delta_time);
-  for (int i = 1; i < sizeof(LED_pins) / sizeof(LED_pins[0]) + 1; i++){
-    digitalWrite(LED_pins[i-1], LOW);
-    digitalWrite(LED_pins[i], HIGH);
-    delay(delta_time);
-  }
-  digitalWrite(LED_pins[sizeof(LED_pins) / sizeof(LED_pins[0])], LOW);
-}
+const int LED_matrix_size_x = 3;
+const int LED_matrix_size_y = 3;
+const int PWM_period_ms = 20;
+
+bool LED_matrix[LED_matrix_size_y][LED_matrix_size_x] = {
+  {true, false, true},
+  {false, true, false},
+  {true, false, true}
+};
+
+uint8_t LED_column_pins[] = {2, 3, 4};
+uint8_t LED_row_pins[] = {5, 6, 7};
 
 void setup() {
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
-  pinMode(12, OUTPUT);
-  
+  Serial.begin(9600);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
 }
 
+int current_step_on_columns = 0;
+unsigned long long prev_ms = 0;
+unsigned long long ms = 0;
+
 void loop() {
-  uint8_t pins[] = {10, 11, 12};
-  LED_arr_by_delay(pins, 2);
-  
+  ms = millis();
+    
+  if(current_step_on_columns >= LED_matrix_size_x){
+    current_step_on_columns = 0;
+  }
+  if (ms - prev_ms >= PWM_period_ms){
+    if (current_step_on_columns == 0){
+      digitalWrite(LED_column_pins[LED_matrix_size_x - 1], LOW);
+    }
+    else{
+      digitalWrite(LED_column_pins[current_step_on_columns-1], LOW);
+    }
+    digitalWrite(LED_column_pins[current_step_on_columns], HIGH);
+
+    current_step_on_columns++;
+    prev_ms = ms;
+  }
 }
