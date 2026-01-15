0ebf5c4cfa483676f09cdf504db5e256af4392fa
zankorobos13 <aruseniys13@yandex.ru>
2026-01-15 20:59:15 +0300

    Система переписана под матрицу 6x9 светодиодов. Матрица игры и матрица светодиодов разделены

--- Tetris.ino
+++ Tetris.ino
@@ -1,19 +1,44 @@
-const int LED_matrix_size_x = 3;
-const int LED_matrix_size_y = 3;
-const int PWM_period_ms = 2;
+const int LED_matrix_size_x = 9;
+const int LED_matrix_size_y = 6;
+const int PWM_period_ms = 1;
 
-bool LED_matrix[LED_matrix_size_y][LED_matrix_size_x] = {
-  {true, true, true},
-  {false, true, false},
-  {false, true, false}
+
+bool play_matrix[LED_matrix_size_x][LED_matrix_size_y] = {
+  {0, 0, 0, 0, 0, 0},
+  {0, 1, 0, 0, 1, 0},
+  {0, 1, 0, 0, 1, 0},
+  {0, 1, 0, 0, 1, 0},
+  {0, 0, 0, 0, 0, 0},
+  {1, 0, 0, 0, 0, 1},
+  {0, 1, 0, 0, 1, 0},
+  {0, 0, 1, 1, 0, 0},
+  {0, 0, 0, 0, 0, 0},
+
 };
 
-uint8_t LED_column_pins[LED_matrix_size_x] = {2, 3, 4};
-uint8_t LED_row_pins[LED_matrix_size_y] = {5, 6, 7};
+bool LED_matrix[LED_matrix_size_y][LED_matrix_size_x] = {
+  {0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0},
+  {0, 0, 0, 0, 0, 0, 0, 0, 0},
+};
+
+uint8_t LED_column_pins[LED_matrix_size_x] = {4, 5, 6, 7, 8, 9, 10, 11, 12};
+uint8_t LED_row_pins[LED_matrix_size_y] = {3, 2, A1, A2, A3, 13};
 
 int current_step_on_columns = 0;
 unsigned long long prev_ms_LED_matrix_control = 0;
 unsigned long long ms = 0;
+
+void LED_matrix_update(){
+  for (int i = 0; i < LED_matrix_size_y; i++){
+    for (int j = 0; j < LED_matrix_size_x; j++){
+      LED_matrix[LED_matrix_size_y-i-1][j] = play_matrix[j][i];      
+    }
+  }
+}
 
 void LED_matrix_control(){
   if(current_step_on_columns >= LED_matrix_size_x){
@@ -21,19 +46,19 @@ void LED_matrix_control(){
   }
   if (ms - prev_ms_LED_matrix_control >= PWM_period_ms){
     if (current_step_on_columns == 0){
-      digitalWrite(LED_column_pins[LED_matrix_size_x - 1], LOW);
+      analogWrite(LED_column_pins[LED_matrix_size_x - 1], 0);
     }
     else{
-      digitalWrite(LED_column_pins[current_step_on_columns-1], LOW);
+      analogWrite(LED_column_pins[current_step_on_columns-1], 0);
     }
-    digitalWrite(LED_column_pins[current_step_on_columns], HIGH);
+    analogWrite(LED_column_pins[current_step_on_columns], 255);
 
     for (int i = 0; i < LED_matrix_size_y; i++){
       if (LED_matrix[i][current_step_on_columns]) {
-        digitalWrite(LED_row_pins[i], HIGH);
+        analogWrite(LED_row_pins[i], 255);
       }
       else{
-        digitalWrite(LED_row_pins[i], LOW);
+        analogWrite(LED_row_pins[i], 0);
       }
     }
 
@@ -45,17 +70,19 @@ void LED_matrix_control(){
 
 void setup() {
   Serial.begin(9600);
-  pinMode(2, OUTPUT);
-  pinMode(3, OUTPUT);
-  pinMode(4, OUTPUT);
-  pinMode(5, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(7, OUTPUT);
+  for (int i = 0; i < LED_matrix_size_x; i++){
+    pinMode(LED_column_pins[i], OUTPUT);
+  }
+  for (int i = 0; i < LED_matrix_size_y; i++){
+    pinMode(LED_row_pins[i], OUTPUT);
+  }
 }
 
 
 
 void loop() {
   ms = millis();
+
+  LED_matrix_update();
   LED_matrix_control();
 }
